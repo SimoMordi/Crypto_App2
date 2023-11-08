85f74307bc2c8ae753520ee0f2be54bd278b87d7
Simo Mordi <126906276+SimoMordi@users.noreply.github.com>
2023-11-08 09:02:15 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,13 @@ app.use(cors({
 app.use(morgan('dev'));
 app.use(helmet());
 
+app.use((req,res,next)=>{
+    if (req.path.startsWith('/server')) {
+        req.url = req.url.replace('/server', ''); // strip /server from the path
+    }
+    next();
+});
+
 // frontend 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 // Read
